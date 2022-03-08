5d8604ed051b26be386c1135b39279320badd9bd
Kain8tp8 <98201571+Kain8tp8@users.noreply.github.com>
2022-03-08 17:25:50 +0500

    Add files via upload

---
+++ Новая папка/out/index.js
@@ -0,0 +1,56 @@
+"use strict";
+//1."Катта ва кичик элемент"
+//let arr: number[] = [11, 2, 5, 3, 5, 55, 10, 12]
+//let max = arr[0]
+//let min = arr[0]
+//
+//for (let i = 0; i < arr.length; i++) {
+//    if (max < arr[i]) {
+//        max = arr[i]
+//    }
+//}
+//for (let i = 0; i < arr.length; i++) {
+//    if (min > arr[i]) {
+//        min = arr[i]
+//    }
+//}
+//console.log('Maxsimum:' + max);
+//console.log('Minimum:' + min);
+//2. "Ток ва жуфт"
+//let arr: number[] = [11, 2, 5, 3, 5, 55, 10, 12]
+//for (let i = 0; i < arr.length; i++) {
+//    if(arr[i]%2==0){
+//       console.log(arr[i]);
+//    }
+//    if(arr[i]%2==1){
+//        console.log(arr[i]);
+//    }
+//}
+//3."*2"
+// let arr: number[] = [11, 2, 5, 3, 5, 55, 10, 12]
+// for (let i = 0; i < arr.length; i++) {
+//    console.log(arr[i]*2);
+// }
+//4.calom isim
+// let arr: string[] = ['Alisher', 'Mansur', 'Abror', 'Nuriddin'];
+// let a: string[] = ['Salom ']
+// for (let i = 0; i < arr.length; i++) {
+//    console.log(a+arr [i]);
+// }
+//5."1 дан 100 гача"
+//let arr:number[]=[]
+//for (let i = 1; i <= 100; i++) {
+//    arr.push (i)
+//}
+//let a=arr
+//console.log(a);
+//for (let i = 0; i < arr.length; i++) {
+//    if(arr[i]%2==0){
+//        let b=arr[i]
+//console.log(b);
+//   }
+//}
+//for (let i = 1; i < 6; i++) {
+//    let c=2**i
+//console.log(c);
+//}
