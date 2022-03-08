1bcac6b4153339459b3a01770333ca960f8e1da8
antiASTRO <firdavssherzodovic@gmail.com>
2022-03-08 15:59:15 +0500

    all

---
+++ out/index.js
@@ -0,0 +1,25 @@
+"use strict";
+// let arr: number[] = [11, 2, 5, 3, 5, 55, 10, 12]
+// let max;
+// let min;
+// for (let i = 0; i < arr.length; i++) {
+//    console.log(arr[i]);
+//    for (let a = 0; a < arr.length; a++) {
+//        console.log( arr[a]);
+//    }
+// }
+// let arr: number[] = [11, 2, 5, 3, 5, 55, 10, 12]
+// for (let i = 0; i < arr.length; i++) {
+//    console.log(arr[i]*2);
+// }
+// let arr: string[] = ['Alisher', 'Mansur', 'Abror', 'Nuriddin'];
+// let a: string[] = ['Salom ']
+// for (let i = 0; i < arr.length; i++) {
+//    console.log(a+arr [i]);
+// }
+let a = [];
+for (let i = 0; i < 100; i++) {
+    let arr;
+}
+a.push =
+;
